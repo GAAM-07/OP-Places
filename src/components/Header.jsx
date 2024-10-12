35be25dd178840afd2cb2d093dc905b78cdb9419
GAAM-07 <gustavoa0503@gmail.com>
2024-10-12 12:45:45 -0500

    added some styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import { Link } from "react-router-dom"
+import { AiOutlineBars } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
 function Header() {
   return (
-    <div>Header</div>
+    <nav>
+        <div className="container nav-container">
+           <Link className="nav-logo" to={"/"} >
+           <h3> Op Paces <span> Blog </span> </h3>
+           </Link>
+           <ul className="nav-menu">
+            <li><Link to={"/profile"}> User </Link> </li>
+            <li><Link to={"/create"}> Create Post </Link> </li>
+            <li><Link to={"/authors"}> Authors </Link> </li>
+            <li><Link to={"/logout"}> Logout </Link> </li>
+            
+           </ul>
+           <button className="nav-toggle-button ">
+           <AiOutlineBars/>
+           <MdClose/>
+           </button>
+       
+        </div>
+
+
+    </nav>
   )
 }
 
