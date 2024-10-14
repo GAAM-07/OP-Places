f190d99aee8b919781fc734b24644201ef266db8
GAAM-07 <gustavoa0503@gmail.com>
2024-10-14 12:08:40 -0500

    error page styles and config

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-
+import { Link } from "react-router-dom"
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <section>
+      <div className="error-center">
+        <h2>404</h2>
+        <h3>this Page not Found !!! </h3>
+        <Link to={"/"} className="btn btn-primary" > Go Back Home </Link>   </div>
+    </section>
   )
 }
 
