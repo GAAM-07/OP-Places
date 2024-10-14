971c5abdc4f3854142911f4011440e1bdd451780
GAAM-07 <gustavoa0503@gmail.com>
2024-10-14 11:44:09 -0500

    added footer styles and configuration categories in it

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <ul className="Container footer-categories">
+        <li><Link to={"/posts/categories/SouthAmerica"}> South America </Link></li>
+        <li><Link to={"/posts/categories/Asia"}> Asia </Link></li>
+        <li><Link to={"/posts/categories/NorthAmerica"}> North America </Link></li>
+        <li><Link to={"/posts/categories/Europe"}> Europe </Link></li>
+        <li><Link to={"/posts/categories/Oceania"}> Oceania </Link></li>
+        <li><Link to={"/posts/categories/Africa"}> Africa </Link></li>
+        <li><Link to={"/posts/categories/CaribbeanCountries"}> Caribbean Countries </Link></li>
+      </ul>
+      <div className="footer-copyright">
+       <small> &copy; Copyright all right reserve to - Gustavo Alvarado And Jovenes Creativos for ROFE Fundation
+        </small>     </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
